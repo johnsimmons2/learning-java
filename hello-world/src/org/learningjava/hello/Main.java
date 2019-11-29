63c42aedbc87844ffefc52846aaa88b6fd9b73f6
Jonathan Simmons <john_simmons2@baylor.edu>
2019-11-28 22:13:26 -0600

    added the first project

---
+++ hello-world/src/org/learningjava/hello/Main.java
@@ -0,0 +1,20 @@
+package org.learningjava.hello; // This is telling Java what package to find the code in.
+// Notice this is in a directory that is org/learningjava/hello.
+
+/**
+ * This is a class declaration, make sure the class name always matches the file name!
+ */
+public class Main {
+
+    /**
+     * Hello World Program! Just prints hello world to the console.
+     * This green text is a comment, it is ignored by compiler so I can write whatever I want.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello World!"); // This is an inline comment, it is also ignored.
+        /*
+        This is another way to write comments on multiple lines. Its good to use the double asterisks though,
+        Because then you can write good descriptions of what each method does.
+         */
+    }
+}
